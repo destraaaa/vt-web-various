2fd136400527ae231b5648e20a88a796e4616e54
Destra Bintang Perkasa <destra.bintang.perkasa@gmail.com>
2014-06-05 14:08:20 +0700

    kelupaan.

---
+++ footer.php
@@ -0,0 +1,8 @@
+<hr>
+<footer>
+	<p class="pull-left">&copy; <a href="#" target="_blank">Pusheen Group and PPL-No-Rata</a> 2014
+		<br> Teknik Informatika ITB
+	</p>
+	<p class="pull-right"> Powered by: <a href="http://usman.it/free-responsive-admin-template">Charisma</a>
+	</p>
+</footer>
